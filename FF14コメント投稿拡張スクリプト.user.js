9161173ebafc9981ab4816c61bd9eb843491d7f5
NohohonNohon <mahorabas@yahoo.co.jp>
2017-09-24 12:41:28 +0900

    ロードストーンのデザイン変更に対応
    ロードストーンのデザイン変更で動作しなくなっため、動作するように修正

--- FF14コメント投稿拡張スクリプト.user.js
+++ FF14コメント投稿拡張スクリプト.user.js
@@ -3,8 +3,8 @@
 // @namespace   nohohon
 // @author      nohohon
 // @description FF14のコメント投稿で表示サイズ変更、プレビュー機能を追加する
-// @include     http://jp.finalfantasyxiv.com/lodestone/character/*
-// @version     1
+// @include     https://jp.finalfantasyxiv.com/lodestone/character/*
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 (function(){
@@ -25,15 +25,15 @@
     */
     function addPreviewButton() {
         //送信ボタンの要素を取得
-        var elemSend = document.evaluate('//*[@id="commentbox_in"]/div/div/div/form/input[2]',
+        var elemSend = document.evaluate('//*[@id="commentbox_in"]/div/form/div[2]/input',
                                         document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
         if(elemSend.snapshotLength){
              //確認ボタンと送信ボタンを並べたHTMLを定義
             var btnHtml = [
-                    '<div class="btn_area" style="margin-top:10px">',
-                    '    <a class="button bt_confirm" id="comment_preview_btn"></a>',
-                    '    <input value="" class="button bt_send" type="submit">',
-                    '</div>'
+                    '<ul class="btn__color__nav--radius">',
+                    '	<li><a id="comment_preview_btn">確認</a></li>',
+                    '	<li class="form__submit"><input type="submit" value="送信" class=""></li>',
+                    '</ul>'
             ].join('');
             //追加したい要素の生成（dummyのdivを使う）
             var divElem       = document.createElement('div');
@@ -57,15 +57,16 @@
         var elemPreviewComment = document.getElementById('preview_comment');
         if(elemPreviewComment == null) {
             elemPreviewComment = document.createElement('div');
-            elemPreviewComment.setAttribute('class','comment')
             elemPreviewComment.setAttribute('id','preview_comment')
             elemPreviewComment.setAttribute('name','preview_comment')
-            var elemCommentList = document.getElementsByClassName('comment_list')[0];
+            var elemCommentList = document.getElementsByClassName('ldst__window')[0];
             elemCommentList.appendChild(elemPreviewComment);
         }
         //プレビュー用のHTMLの更新とページ内移動
         elemPreviewComment.innerHTML = previewHtml;
-        window.location.href = '#preview_comment';
+        setTimeout(function(){
+            window.location.href = '#preview_comment';
+        }, 100);
     }
 
     /** 
@@ -77,33 +78,38 @@
         var date = new Date();
         var dateString = date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + date.getDate()).slice(-2)+
                                 ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
-        var elemPlayerId = document.evaluate('//*[@id="commentbox_in"]/div/div/div/form/div/div/div/div[1]',
+        var elemPlayerName = document.evaluate('//*[@id="community"]/div[4]/div[2]/div[2]/div[4]/div[2]/a[1]/div/p[1]',
+                                        document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null ).snapshotItem(0);
+        var elemPlayerServer = document.evaluate('//*[@id="community"]/div[4]/div[2]/div[2]/div[4]/div[2]/a[1]/div/p[2]',
                                         document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null ).snapshotItem(0);
         var faceRadioList = document.getElementsByName('face_emotion_id');
         var srcFaceImage;
         for(var i=0; i<faceRadioList.length; i++){
             if (faceRadioList[i].checked) {
-                srcFaceImage = faceRadioList[i].parentNode.children[0].getAttribute('src');
+                srcFaceImage = faceRadioList[i].parentNode.children[1].children[0].getAttribute('src');
                 break;
             }
         }
         //HTMLを生成
         var previewHtml = [
-                '<div class="clearfix">',
-                '    <div class="thumb">',
-                '        <img src="',srcFaceImage,'" alt="" height="50" width="50">',
-                '    </div>',
-                '    <div class="balloon_header"><div class="balloon_footer"><div class="balloon_body"><div class="balloon_body_inner">',
-                elemComment.value.replace(/[\n\r]/g, '<br />'),
-                '    </div></div></div></div>',
-                '</div>',
-                '<div class="tr relative">',
-                '   <div style="color:#ff0000;margin-top:7px;margin-bottom:4px;">コメント確認</div>',
-                '   <div class="player_id">',elemPlayerId.innerHTML,'</div>',
-                '    <span class="ml5 pr6"> ',
-                '        <span>',dateString,'</span>',
-                '    </span>',
-                '</div>',
+                    '<div class="entry">',
+                    '	<a class="entry__link">',
+                    '		<div class="entry__chara__face">',
+                    '			<img src="',srcFaceImage,'" alt="" height="50" width="50">',
+                    '		</div>',
+                    '		<div class="entry__box">',
+                    '			<p class="entry__name">',elemPlayerName.innerHTML,'</p>',
+                    '			<p class="entry__world">',elemPlayerServer.innerHTML,'</p>',
+                    '			<time class="entry__time--comment"> ',
+                    '			<span>',dateString,'</span>',
+                    '			</time>',
+                    '		</div>',
+                    '	</a>',
+                    '</div>',
+                    '<div class="thread__comment__body">',
+                    elemComment.value.replace(/[\n\r]/g, '<br />'),
+                    '</div>',
+                    '<div style="color:#ff0000;margin-top:7px;margin-bottom:4px;margin-left:16px;">コメント確認</div>',
             ].join('');
             return previewHtml;
     }
